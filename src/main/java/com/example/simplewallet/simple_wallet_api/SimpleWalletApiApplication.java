56914916ee01fe34b6c16fefa5f4457cc72d796f
Patrick <patrick.fernandes@estudante.ufla.br>
2026-06-04 21:36:29 -0300

    feat: primeiro commit

---
+++ src/main/java/com/example/simplewallet/simple_wallet_api/SimpleWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplewallet.simple_wallet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplewallet/simple_wallet_api/SimpleWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplewallet.simple_wallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
