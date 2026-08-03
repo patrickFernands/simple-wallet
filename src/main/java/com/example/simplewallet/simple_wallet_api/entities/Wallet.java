008fdc8fa0f0386d78408661f7fd9fdc82e9c5d5
Patrick <patrick.fernandes@estudante.ufla.br>
2026-08-03 19:14:35 -0300

    feat: implementa testes unitários

--- src/main/java/com/example/simplewallet/simple_wallet_api/entities/Wallet.java
+++ src/main/java/com/example/simplewallet/simple_wallet_api/entities/Wallet.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 
 @Entity
 @Table(name = "tb_wallet")
